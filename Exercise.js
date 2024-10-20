5981cdd8887a514442ffd76b2c60de060c13e8c4
antonniga7 <84639635+antonniga7@users.noreply.github.com>
2024-10-19 22:24:07 -0500

    Add files via upload

---
+++ Exercise.js
@@ -0,0 +1,18 @@
+let cuenta = [100];
+let a = 1;
+    for (let i = 0; i < cuenta; i++){
+        i++
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Fizzbuzz')
+    } else if (i % 3 === 0) {
+        console.log('fizz')
+    } else if (i % 5 === 0) {
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
+    }
+    
+
+
+
